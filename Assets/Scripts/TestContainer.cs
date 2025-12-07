a268f079698e390ad3eb30991c648f82df8c1cae
amenonegames <100146683+amenonegames@users.noreply.github.com>
2025-12-07 19:26:02 +0900

    sample case

---
+++ Assets/Scripts/TestContainer.cs
@@ -0,0 +1,76 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+using VitalRouter;
+using VitalRouter.VContainer;
+
+namespace DefaultNamespace
+{
+    public class TestContainer : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.RegisterVitalRouter( router =>
+            {
+                router.FanOut(parallelGroup =>
+                {
+                    parallelGroup.Ordering = CommandOrdering.Parallel;
+                    parallelGroup.Map<ParallelPresenter>();
+                });
+
+                router.FanOut(sequentialGroup =>
+                {
+                    sequentialGroup.Ordering = CommandOrdering.Sequential;
+                    sequentialGroup.Map<SequentialPresenter>();
+                });
+            });
+
+            builder.RegisterEntryPoint<Entrypoint>();
+        }
+    }
+
+    public class Entrypoint : IStartable
+    {
+        private readonly ICommandPublisher _commandPublisher;
+        public Entrypoint(ICommandPublisher commandPublisher)
+        {
+            _commandPublisher = commandPublisher;
+        }
+
+
+        public void Start() => _commandPublisher.PublishAsync(new SequenceMsg());
+    }
+
+    public struct ParallelMsg:ICommand
+    {}
+    public struct SequenceMsg:ICommand
+    {}
+
+    [Routes]
+    public partial class ParallelPresenter
+    {
+        [Route]
+        void On(ParallelMsg parallelMsg)
+        {
+            Debug.Log("parallelMsg is published");
+        }
+    }
+    [Routes]
+    public partial class SequentialPresenter
+    {
+        private readonly ICommandPublisher _commandPublisher;
+
+        public SequentialPresenter(ICommandPublisher commandPublisher)
+        {
+            _commandPublisher = commandPublisher;
+        }
+        [Route]
+        async UniTask On(SequenceMsg sequenceMsg)
+        {
+            Debug.Log("sequenceMsg is published. start processing");
+            await _commandPublisher.PublishAsync(new ParallelMsg());
+            Debug.Log("sequenceMsg is published. finish processing");
+        }
+    }
+}
